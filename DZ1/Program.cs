2d8096da7574d6e3354f52bf026befcc030188f0
Anna <radiant2958@gmail.com>
2022-10-16 21:28:12 +0300

    задача2

---
+++ DZ1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2
+Console.WriteLine("введите число");
+int number11 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число");
+int number22 = Convert.ToInt32(Console.ReadLine());
+if (number11 > number22)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+
+else if (number11 < number22)
+{
+    Console.WriteLine("Второе число больше первого");
+}
+
+else
+{
+    Console.WriteLine("числа равны");
+}
