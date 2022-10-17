c082c051de9e89436782ee352e44403d985f383b
Anna <radiant2958@gmail.com>
2022-10-17 21:35:57 +0300

    задача8

---
+++ DZ4/Program.cs
@@ -0,0 +1,14 @@
+﻿// задача8
+Console.WriteLine("введите число");
+int number8 = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i<=number8; i++)
+{
+       if (i%2==0)
+       {
+        Console.WriteLine(i);
+
+
+       }
+       
+}
