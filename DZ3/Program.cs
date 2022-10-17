929647c9fe424867d1a5367473ca1c35b8b8b872
Anna <radiant2958@gmail.com>
2022-10-17 21:06:16 +0300

    задача6

---
+++ DZ3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 6
+Console.WriteLine("введите число");
+int number6 = Convert.ToInt32(Console.ReadLine());
+if (number6%2 == 0)
+{
+    Console.WriteLine("число четное");
+}
+else{
+    Console.WriteLine("число не четное");
+}
