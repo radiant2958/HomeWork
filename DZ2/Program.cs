1632ad50796ae14c9ea57926cbaf7ac9ac7e9d11
Anna <radiant2958@gmail.com>
2022-10-17 20:59:55 +0300

    задача4

---
+++ DZ2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4
+Console.WriteLine("введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1>number2 & number1> number3)
+{
+    Console.WriteLine(number1);
+}
+
+else if (number2>number1 & number2>number3)
+{
+    Console.WriteLine(number2);
+}
+
+else
+{
+    Console.WriteLine(number3);
+}
