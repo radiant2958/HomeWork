0caa4a0641836dbba2c0d1c5a391ac5adf96ab08
Anna <radiant2958@gmail.com>
2022-10-18 22:00:08 +0300

    задача10

---
+++ DZ5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10
+int SecondDigit(int number)
+{
+    int lastnumber = number/10;
+    int  secondnumber = lastnumber%10;
+
+
+    return secondnumber;
+
+}
+
+Console.WriteLine(SecondDigit(345));
+Console.WriteLine(SecondDigit(22));
