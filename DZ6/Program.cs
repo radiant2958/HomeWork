0bdb657d7da25af36b2e3b5c0cf8810520f4c664
Anna <radiant2958@gmail.com>
2022-10-19 22:51:49 +0300

    написали цикл перевода числа в массив

---
+++ DZ6/Program.cs
@@ -0,0 +1,34 @@
+﻿// задача 13
+/*int ThirdDigit(int number)
+{
+    
+    if (number>99 )
+    {
+        number/=10;
+        int d=number%10;
+        return d; 
+    }
+       
+     
+}
+Console.WriteLine(ThirdDigit(345));
+
+*/
+
+int Fun(int number)
+{
+    int[] array = new int[3];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+      int lastValue = number % 10;
+      array[i] = lastValue;
+      number = number / 10;
+    }
+    
+    
+return array[0];
+
+}
+
+Console.WriteLine(Fun(56));
