b79056bf43e6e31dc0511f59f64125f38c09a85b
Anna <radiant2958@gmail.com>
2022-10-20 23:34:22 +0300

    эксперемент

---
+++ DZ7/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15
+ bool Weekday (int number)
+ {
+    Console.WriteLine("сегодня выходной?");
+    
+    if (number ==6 | number==7)
+    {
+        bool a = true;
+    }
+    else
+    {
+        bool a=false;
+      
+    }
+    return a;
+ 
+ }
+
+Weekday(6);
