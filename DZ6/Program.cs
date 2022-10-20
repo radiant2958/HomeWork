9bd976434dde39f1f379f876163fe977d7dc9274
Anna <radiant2958@gmail.com>
2022-10-20 22:53:02 +0300

    написали цикл2

--- DZ6/Program.cs
+++ DZ6/Program.cs
@@ -1,34 +1,20 @@
 ﻿// задача 13
-/*int ThirdDigit(int number)
-{
-    
-    if (number>99 )
-    {
-        number/=10;
-        int d=number%10;
-        return d; 
-    }
-       
-     
-}
-Console.WriteLine(ThirdDigit(345));
-
-*/
-
-int Fun(int number)
+int ThirdDigit(int number)
 {
     int[] array = new int[3];
 
-    for (int i = 0; i < array.Length; i++)
-    {
-      int lastValue = number % 10;
-      array[i] = lastValue;
-      number = number / 10;
-    }
+        for (int i = 0; i < array.Length; i++)
+        {
+        int lastValue = number % 10;
+        array[i] = lastValue;
+        number = number / 10;
+        }
+      
+      array=Array.Reverse(array);
+      
+      
+      return array[2];
     
-    
-return array[0];
-
 }
 
-Console.WriteLine(Fun(56));
+Console.WriteLine(ThirdDigit(678));
